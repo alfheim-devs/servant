75decc749a797f0804513308e1571b93c5be6b7a
gabehellz <gabehellz@proton.me>
2025-08-18 23:47:50 -0300

    feat: estrutura básica do bot (#4)

---
+++ src/commands/Experimental/test.ts
@@ -0,0 +1,17 @@
+import { Discord, Guild, Slash } from "discordx";
+import { Category } from "@discordx/utilities";
+import { CommandInteraction } from "discord.js";
+
+@Discord()
+@Category("Experimental")
+export default class TestCommand {
+    @Slash({
+        name: "test",
+        description: "Just a test command",
+    })
+    async test(interaction: CommandInteraction): Promise<void> {
+        await interaction.reply({
+            content: `This is a test, ${interaction.member}!`,
+        });
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("Hello, World!");
+import "reflect-metadata";
+import "dotenv/config";
+import { container } from "tsyringe";
+import { Client, DIService, tsyringeDependencyRegistryEngine } from "discordx";
+import { importx, dirname } from "@discordx/importer";
+import { GatewayIntentBits } from "discord.js";
+
+DIService.engine = tsyringeDependencyRegistryEngine.setInjector(container);
+
+const DISCORD_GUILD_ID = process.env.DISCORD_GUILD_ID;
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN || "";
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+    silent: false,
+    botGuilds: DISCORD_GUILD_ID ? [DISCORD_GUILD_ID] : [],
+});
+
+async function main() {
+    await importx(`${dirname(import.meta.url)}/{events,commands}/**/*.{ts,js}`);
+    await client.login(DISCORD_TOKEN);
+}
+
+main();
