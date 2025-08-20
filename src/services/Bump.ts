d3f7aa74d81bbdfab4f5793704ea71a298bd459d
gabehellz <gabehellz@proton.me>
2025-08-20 18:10:24 -0300

    feat(bump): adicionando lembrete de bump (#12)

--- src/services/Bump.ts
+++ src/services/Bump.ts
@@ -3,6 +3,8 @@ import { Database } from "./Database.js";
 import { Embed, Message, TextChannel } from "discord.js";
 
 const DISBOARD_BOT_ID = process.env.DISBOARD_BOT_ID || "";
+const BUMP_ROLE_ID = process.env.BUMP_ROLE_ID || "";
+const TWO_HOURS = 60 * 60 * 1000;
 
 @singleton()
 export class Bump {
@@ -32,10 +34,21 @@ export class Bump {
                 const bumps = user?.bumps + 1;
                 void this.database.incrementUserBump(userId, bumps);
                 await message.react("👍");
+
                 await channel.send(
                     `<@${user.id}>, você acabou de dar bump! Agora você tem ${bumps} bumps!`,
                 );
+
+                await this.timeout(TWO_HOURS);
+
+                await channel.send(
+                    `<@&${BUMP_ROLE_ID}>, o bump está pronto! Use o comando </bump:947088344167366698>`,
+                );
             }
         }
+    }
+
+    timeout(milliseconds: number) {
+        return new Promise((resolve) => setTimeout(resolve, milliseconds));
     }
 }
