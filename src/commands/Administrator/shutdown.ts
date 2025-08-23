dbb705a234522a6448d08c920d9e1ef251ab515c
rahvax <rahvax@proton.me>
2025-08-23 14:23:45 -0300

    remove try-catch

--- src/commands/Administrator/shutdown.ts
+++ src/commands/Administrator/shutdown.ts
@@ -1,3 +1,6 @@
+/*
+  Comando temporário de finalização do cliente
+*/
 import { Discord, Slash } from "discordx";
 import { Category } from "@discordx/utilities";
 import { CommandInteraction } from "discord.js";
@@ -12,10 +15,6 @@ export default class ShutdownCommand {
     async shutdown(interaction: CommandInteraction): Promise<void> {
         if (interaction.member?.user.id != "314858301726785546") return;
         await interaction.reply({ content: `Encerrando o bot.` });
-        try {
-            await interaction.client.destroy();
-        } catch (err) {
-            console.error("Erro ao tentar desligar o cliente: ", err);
-        }
+        await interaction.client.destroy().catch((err) => console.log(err));
     }
 }
